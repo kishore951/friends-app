851ef191ffa0747a35dec4918ab364be213feba7
kishore951 <kishoresakthivel951@gmail.com>
2022-10-13 20:24:23 +0530

    over

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { AddDataComponent } from './add-data/add-data.component';
 import { RouterModule, Routes } from '@angular/router';
 import { ViewFriendsDataComponent } from './view-friends-data/view-friends-data.component';
+import { HttpClientModule } from '@angular/common/http'
 
 const appRoutes:Routes=[
   {
@@ -26,7 +27,8 @@ const appRoutes:Routes=[
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
