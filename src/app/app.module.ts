181cfcc5b0080d9167d01ebbece70b41fbee929c
kishore951 <kishoresakthivel951@gmail.com>
2022-10-13 20:14:41 +0530

    for

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,22 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AddDataComponent } from './add-data/add-data.component';
 import { RouterModule, Routes } from '@angular/router';
+import { ViewFriendsDataComponent } from './view-friends-data/view-friends-data.component';
 
 const appRoutes:Routes=[
   {
     path:"",component:AddDataComponent
+  },
+  {
+    path:"search",component:ViewFriendsDataComponent
   }
 ]
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    AddDataComponent
+    AddDataComponent,
+    ViewFriendsDataComponent
   ],
   imports: [
     BrowserModule,
